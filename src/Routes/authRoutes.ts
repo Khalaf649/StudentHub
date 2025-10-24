9a4c5a0f9d234ac907d90a5950dde8b74b1217b6
Abdelrahman Khalaf <abdlrhmankhalaf566@gmail.com>
2025-10-24 22:27:01 +0300

    update getInfo for studnet

--- src/Routes/authRoutes.ts
+++ src/Routes/authRoutes.ts
@@ -3,11 +3,9 @@ import { login, register } from "../Controllers/authController";
 import { loginValidation } from "../Validation/LoginValidator";
 import StudentValidator from "../Validation/StudentValidator";
 import { validationMiddleware } from "../Middlewares/validationMiddleware";
-const router=Router();
+const router = Router();
 // Middleware to authenticate and differentiate roles
-router.post("/login",loginValidation,validationMiddleware,login);
-router.post("/register",StudentValidator,validationMiddleware,register);
-
-
+router.post("/login", loginValidation, validationMiddleware, login);
+router.post("/register", StudentValidator, validationMiddleware, register);
 
 export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-const port=process.env.PORT;
+import { setupSwagger } from "./swagger";
+const PORT = process.env.PORT;
 import bodyparser from "body-parser";
 import StudentRouter from "./Routes/studentRoutes";
 import TeacherRouter from "./Routes/teacherRoutes";
 import AuthRouter from "./Routes/authRoutes";
 import cors from "cors";
-const app=express();
+const app = express();
+
 app.use(cors());
 app.use(bodyparser.json());
-app.use("/student",StudentRouter);
-app.use("/teacher",TeacherRouter);
-app.use("/auth",AuthRouter);
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+app.use("/student", StudentRouter);
+app.use("/teacher", TeacherRouter);
+app.use("/auth", AuthRouter);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`📘 Swagger Docs available at http://localhost:${PORT}/api-docs`);
 });
