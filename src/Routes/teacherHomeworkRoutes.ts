68eb822c59ce75ed60393f6834077f74614109af
Abdelrahman Khalaf <abdlrhmankhalaf566@gmail.com>
2026-01-15 15:09:28 +0200

    fix bug in teacherroutes

--- src/Routes/teacherHomeworkRoutes.ts
+++ src/Routes/teacherHomeworkRoutes.ts
@@ -7,7 +7,7 @@ import {
 import authMiddleware from "../Middlewares/authMiddleware.js";
 import roleMiddleware from "../Middlewares/roleMiddleware.js";
 import homeworkValidator from "../Validation/homeworkValidator.js";
-import studentHomeworkValidator from "../Validation/StudentHomeworkValidator.js";
+import studentHomeworkValidator from "../Validation/studentHomeworkValidator.js";
 import { validationMiddleware } from "../Middlewares/validationMiddleware.js";
 
 const router = Router();
