bf26de19ea55a4ac790aa969b6a279f206d7ecb0
Abdelrahman Khalaf <abdlrhmankhalaf566@gmail.com>
2026-01-17 23:54:13 +0200

    add validation in the auth routes

--- src/Routes/authRoutes.ts
+++ src/Routes/authRoutes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
-// import loginValidation from "../validation/loginValidator.ts";
-// import StudentValidator from "../validation/studentValidator.ts";
+import loginValidator from "../validation/loginValidator.ts";
+import studentValidator from "../validation/studentValidator.ts";
 import { validationMiddleware } from "../Middlewares/validationMiddleware.ts";
 import { login, registerStudent } from "../Controllers/authController.ts";
 
 const router = Router();
 // Middleware to authenticate and differentiate roles
-router.post("/login", validationMiddleware, login);
-router.post("/register", validationMiddleware, registerStudent);
+router.post("/login", loginValidator, validationMiddleware, login);
+router.post(
+  "/register",
+  studentValidator,
+  validationMiddleware,
+  registerStudent,
+);
 
 export default router;
